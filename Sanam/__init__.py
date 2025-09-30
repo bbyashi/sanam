7c75f19b8c3d14d6ba8c65108783767ef50182dd
bbyashi <chikubhardwaj1525@gmail.com>
2025-09-30 17:12:09 +0000

    done

---
+++ Sanam/__init__.py
@@ -0,0 +1,26 @@
+from Sanam .core.bot import Aviax
+from Sanam .core.dir import dirr
+from Sanam .core.git import git
+from Sanam .core.userbot import Userbot
+from Sanam .misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Aviax()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
